227483b4df0f444e670515d247ed31cec5528911
Natanaelvich <taelima1997@gmail.com>
2023-04-09 22:18:08 -0300

    add components to index

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,1 +1,7 @@
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
+export * from './components/Avatar'
+export * from './components/Button'
+
 export * from './styles'
