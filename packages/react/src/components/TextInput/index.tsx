3b17ef2539a3f581a64153f78511cf69d44ce68a
Natanaelvich <taelima1997@gmail.com>
2023-04-10 22:48:34 -0300

    add component TextInput

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,19 @@
+import { ComponentProps, forwardRef, ElementRef } from 'react'
+import { Input, Prefix, TextInputContainer } from './styles'
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+}
+
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ prefix, ...props }: TextInputProps, ref) => {
+    return (
+      <TextInputContainer>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
+        <Input ref={ref} {...props} />
+      </TextInputContainer>
+    )
+  },
+)
+
+TextInput.displayName = 'TextInput'
